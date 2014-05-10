c4e6c7604971921973689a1b32f6ed022ea18539
Joel Cantoral <cs.joel.09@gmail.com>
2014-05-09 22:41:46 -0600

    Using server version of brain js node module

--- web.js
+++ web.js
@@ -5,22 +5,64 @@
 var ejs = require('ejs');
 var express = require('express');
 var fs = require('fs');
+var http = require('http');
 
 var app = express();
 
+var assert = require("assert"),
+    brain = require("brain"),
+    bodyParser = require('body-parser');
+
+//Neural network definition
+var net = new brain.NeuralNetwork({
+  hiddenLayers: [10,10],
+  momentum:0.7
+});
 
 app.engine('html', ejs.renderFile);
 app.set('views', __dirname);
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser({limit:'50mb'}));
 
 app.get('/', function (req, res) {
   res.render('app.html');
 });
 
-app.get('/random', function(req, res) {
-  //process.stdout.write(__dirname+"/1.jpg");
+app.post('/train', function(req, res){
+  var trainData = req.body.entrenamiento;
+  var epochs = req.body.epochs;
+  net.train(trainData, {
+    iterations:epochs,
+    callbackPeriod:1,
+    callback:function(m){
+      console.log(m);
+      console.log('end training');
+      res.end('OK');
+    }
+  });
+});
 
+app.get('/getNet', function(req, res){
+  res.end(JSON.stringify(net.toJSON()));
+});
+
+app.post('/test', function(req, res){
+  var prueba = req.body.prueba;
+  var result = net.run(prueba);
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(result));
+});
+
+app.get('/random', function(req, res) {
   var fileNumber = Math.round(Math.random()*27) + 1;
+  fs.readFile('/images/'+fileNumber+'.png', function(data) {
+    res.contentType('image/png');
+    res.end('/images/'+fileNumber+'.png', 'binary');
+  });
+});
+
+app.get('/image/:fileNumber', function(req, res) {
+  fileNumber = req.param('fileNumber');
   fs.readFile('/images/'+fileNumber+'.png', function(data) {
     res.contentType('image/png');
     res.end('/images/'+fileNumber+'.png', 'binary');
